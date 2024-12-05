ba757f05b974ad245c73845d8092b9a7e2486488
Jorge <jorgaresp@gmail.com>
2024-12-05 18:01:09 +0100

    Initial commit

---
+++ src/chatbot/Main.java
@@ -0,0 +1,10 @@
+package chatbot;
+
+public class Main {
+    public static void main(String[] args) {
+        String botName = "Marty";
+        int birthYear = 2024;
+
+        System.out.printf("Hello! My name is " + botName + "\n"+ "I was created in " + birthYear);
+    }
+}
